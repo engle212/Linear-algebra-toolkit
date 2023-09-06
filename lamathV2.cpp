df041c516e86697dfec33e9ecf2ee3c0a75dc3ce
engle212 <engle.212@buckeyemail.osu.edu>
2023-09-06 19:05:16 -0400

    Implemented compatibility for any shape of matrix

--- lamathV2.cpp
+++ lamathV2.cpp
@@ -2,8 +2,9 @@
 #include <iostream>
 #include <cmath>
 #include <algorithm>
+#include <vector>
 
-using namespace std;
+//using namespace std;
 
 bool compare(float a, float b, float epsilon = 0.0001f) {
     return (fabs(a - b) < epsilon);
@@ -11,7 +12,7 @@ bool compare(float a, float b, float epsilon = 0.0001f) {
 
 class Matrix {
     private:
-        string matrix;
+        std::vector<float> matrix;
         int rows;
         int columns;
         void setDim(int m, int n) {
@@ -19,20 +20,46 @@ class Matrix {
             columns = n;
             matrix.resize(rows*columns, '0');
         }
-        pair<int, int> getDim() {
-            return make_pair(rows, columns);
-        }
+        
     public:
         Matrix(int m, int n) {
             // Set dimensions of matrix
             setDim(m, n);
         }
-        Matrix(int m, int n, vector< vector<int> > &vect) {
+        Matrix(std::vector<std::vector<float>> &vect) {
             // Set dimensions of matrix
-            setDim(m, n);
-            // Load vector values into matrix string
-            for (int i = 0; i < matrix.length(); i++) {
-                
+            setDim(vect.size(), vect.at(0).size());
+            // Load vector values into matrix
+            for (int i = 0; i < matrix.size(); i++) {
+                int c = i % columns;
+                // Set matrix at position i to value from 2D vector
+                matrix[i] = vect.at((i - c) / columns).at(c);
             }
         }
+        std::string toString() {
+            std::string str = "{";
+            // Cycle through matrix and display values
+            for (int i = 0; i < matrix.size(); i++) {
+                if (i != 0 && i % columns == 0) {
+                    str += "}\n{";
+                }
+                str += "[" + std::to_string(matrix.at(i)) + "]";
+            }
+            str +="}";
+            return str;
+        }
+        std::pair<int, int> getDim() {
+            return std::make_pair(rows, columns);
+        }
+};
+
+int main() {
+    std::vector<std::vector<float>> v = {{1.0f, 3.0f, 4.0f}, {1.0f, 1.0f, 0.0f}, {2.0f, 2.0f, 2.0f}};
+    Matrix m(v);
+
+    //m.echelon();
+
+    std::string str = m.toString();
+    std::cout << str;
+    return 0;
 }

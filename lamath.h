c46d162c4b5e89c5eba3117851e876a555f651b1
engle212 <engle.212@buckeyemail.osu.edu>
2024-01-28 20:45:29 -0500

    Fixed indentation on header file.

--- lamath.h
+++ lamath.h
@@ -1,26 +1,25 @@
 #pragma once
 #ifndef LAMATH_H
-#define LAMATH_H
+  #define LAMATH_H
+#endif
 #include <string>
 
 using namespace std;
 
 class Matrix {
-    private:
-        // Populate arr by passing in 2D array
-        void populate(int arr[][3], int rows);
-        // Adding, subtracting, scaling, and swapping rows
-        void add(int r1, int r2);
-        void subtract(int r1, int r2);
-        void scaleRow(int row, int scalar);
-        void swap(int r1, int r2);
+  private:
+    // Populate arr by passing in 2D array
+    void populate(int *arr, int rows);
+    // Adding, subtracting, scaling, and swapping rows
+    void add(int r1, int r2);
+    void subtract(int r1, int r2);
+    void scaleRow(int row, int scalar);
+    void swap(int r1, int r2);
 
-    public:
-        // Constructor
-        Matrix(int arr[][3], int rows);
-        string toString();
-        void Echelon();
-        void ReducedEchelon();
-
+  public:
+    // Constructor
+    Matrix(int *arr, int rows);
+    string toString();
+    void Echelon();
+    void ReducedEchelon();
 };
-#endif
